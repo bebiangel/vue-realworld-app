5fe42d6aab64d6735f39810a68b6c7403fce89c3
jaewoo <rossoneri1012@gmail.com>
2020-04-28 20:48:51 +0900

    feat: router 셋팅

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,55 @@
 <template>
-    
+    <nav class="navbar navbar-light">
+        <div class="container">
+            <router-link
+                    class="navbar-brand"
+                    :to="{name:'home'}">
+                conduit
+            </router-link>
+            <ul class="nav navbar-nav pull-xs-right">
+                <li class="nav-item">
+                    <router-link
+                            class="nav-link"
+                            active-class="active"
+                            exact
+                            :to="{name:'home'}"
+                    >
+                        Home
+                    </router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link
+                            class="nav-link"
+                            active-class="active"
+                            exact
+                            :to="{name:'new', path:'/new'}"
+                    >
+                        <i class="ion-compose"/>&nbsp;New Post
+                    </router-link>
+
+                </li>
+                <li class="nav-item">
+                    <router-link
+                            class="nav-link"
+                            active-class="active"
+                            exact
+                            :to="{name:'settings', path:'/settings'}"
+                    >
+                        <i class="ion-gear-a"/>&nbsp;Settings
+                    </router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link
+                            class="nav-link"
+                            active-class="active"
+                            exact
+                            :to="{name:'signup', path:'/signup'}">
+                        Sign up
+                    </router-link>
+                </li>
+            </ul>
+        </div>
+    </nav>
 </template>
 
 <script>
